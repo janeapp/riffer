a0e6bf6eb16ae17472c429ae36b0a327efb8d2ee
Jake Bottrall <jakebottrall@gmail.com>
2025-12-18 14:38:13 -0800

    intiailize project

---
+++ lib/riffer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "riffer/version"
+
+module Riffer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/riffer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Riffer do
+  it "has a version number" do
+    expect(Riffer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
