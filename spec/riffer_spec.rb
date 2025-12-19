d20147c6bd1b3d423f8915297ef5896b993d6cec
Jake Bottrall <jakebottrall@gmail.com>
2025-12-18 20:32:12 -0800

    Fix RSpec expectations for version check and utility test

--- spec/riffer_spec.rb
+++ spec/riffer_spec.rb
@@ -2,10 +2,10 @@
 
 RSpec.describe Riffer do
   it "has a version number" do
-    expect(Riffer::VERSION).not_to be nil
+    expect(Riffer::VERSION).not_to be_nil
   end
 
   it "does something useful" do
-    expect(true).to eq(true)
+    expect(true).to be(true)
   end
 end
