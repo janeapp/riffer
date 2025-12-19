482bbbd0f2cceaa040a354ccb0429579064c58e7
Jake Bottrall <jakebottrall@gmail.com>
2025-12-18 20:32:30 -0800

    Remove unused test for utility functionality in Riffer spec

--- spec/riffer_spec.rb
+++ spec/riffer_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Riffer do
   it "has a version number" do
     expect(Riffer::VERSION).not_to be_nil
   end
-
-  it "does something useful" do
-    expect(true).to be(true)
-  end
 end
