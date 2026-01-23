b0537a4a36971704522b0e3ad80e291f44a9c833
Jake Bottrall <75273444+bottrall@users.noreply.github.com>
2026-01-23 15:53:23 -0800

    feat: update configuration structure for Anthropic and OpenAI

--- lib/riffer/config.rb
+++ lib/riffer/config.rb
@@ -9,20 +9,28 @@
 #   Riffer.config.amazon_bedrock.region = "us-east-1"
 #   Riffer.config.amazon_bedrock.api_token = "..."
 #
+#   Riffer.config.anthropic.api_key = "sk-ant-..."
+#
 class Riffer::Config
-  # OpenAI configuration (Struct with +api_key+).
-  #
-  # Returns Struct.
-  attr_reader :openai
-
   # Amazon Bedrock configuration (Struct with +api_token+ and +region+).
   #
   # Returns Struct.
   attr_reader :amazon_bedrock
 
+  # Anthropic configuration (Struct with +api_key+).
+  #
+  # Returns Struct.
+  attr_reader :anthropic
+
+  # OpenAI configuration (Struct with +api_key+).
+  #
+  # Returns Struct.
+  attr_reader :openai
+
   # Initializes the configuration.
   def initialize
-    @openai = Struct.new(:api_key).new
     @amazon_bedrock = Struct.new(:api_token, :region).new
+    @anthropic = Struct.new(:api_key).new
+    @openai = Struct.new(:api_key).new
   end
 end
